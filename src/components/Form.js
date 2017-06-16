1f906971f68aaa4549eeda513c331a5b6bef9431
github username <github email address>
2017-06-16 15:42:08 -0400

    form and api post function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,43 +1,41 @@
 import React, { Component } from  'react'
 import { Route, Link } from 'react-router-dom'
 
-export default class From extends Component {
+export default class Form extends Component {
   constructor() {
     super()
 
     this.state = {
       name: "",
-      days_until_expiration: "",
+      days: "",
       quantity: "",
       category_id: ""
     }
 
     this.handleChange = this.handleChange.bind(this)
-    // this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
   }
 
   handleChange(event) {
-    debugger
-    this.setState(function(previousState) {
-      return {
-        [event.target.name]: event.target.value
-      }
-    })
+    this.setState({
+      [event.target.name]: event.target.value
+    }, console.log(this.state.name))
   }
 
-  // handleSubmit(event) {
-  //   event.preventDefault()
-  //
-  //
-  // }
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.onSubmit(event.target.children)
+  }
 
   render() {
     return(
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Days until expiration" />
-        <input type="text" placeholder="Quantity" />
-        <input type="text" placeholder="Category" />
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" name="name" placeholder="Name" value={this.state.name} onChange={this.handleChange} /><br/>
+        <input type="text" name="days" placeholder="Days until expiration" value={this.state.days} onChange={this.handleChange} /><br/>
+        <input type="text" name="quantity" placeholder="Quantity" value={this.state.quantity} onChange={this.handleChange}  /><br/>
+        <input type="text" name="category_id" placeholder="Category" value={this.state.category_id} onChange={this.handleChange} /><br/>
+        <input type="submit" value="Submit" />
       </form>
     )
   }
