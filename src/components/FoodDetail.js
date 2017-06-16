0b7c1ff846927bd12d3da2564d362fd6f3a686fc
github username <github email address>
2017-06-16 16:26:28 -0400

    primordial routes working-ish

--- src/components/FoodDetail.js
+++ src/components/FoodDetail.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
-export default class FoodDetail extends Component {
-  constructor(){
-    super()
-    this.state = {
 
+
+export default class FoodDetail extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      food: this.props.food
     }
+  }
+  render() {
+    return (
+      <div className="row">
+      Details!
+      {this.state.food.name}
+      </div>
+    )
   }
 }
