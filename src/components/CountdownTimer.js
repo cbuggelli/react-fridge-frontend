c0971f50951f60e745a32beb8747d496aa535a9a
github username <github email address>
2017-06-17 21:41:31 -0400

    timer works

--- src/components/CountdownTimer.js
+++ src/components/CountdownTimer.js
@@ -7,7 +7,28 @@ export default class CountdownTimer extends Component {
       timeRemaining: 0
     }
     this.tick = this.tick.bind(this)
+    this.days = this.days.bind(this)
+    this.hours = this.hours.bind(this)
+    this.minutes = this.minutes.bind(this)
+    this.seconds = this.seconds.bind(this)
   }
+
+  days() {
+    return Math.floor(this.state.timeRemaining / (3600*24))
+  }
+
+  hours() {
+    return Math.floor(this.state.timeRemaining / 3600)
+  }
+
+  minutes() {
+    return Math.floor((this.state.timeRemaining - (this.hours() * 3600)) / 60)
+  }
+
+  seconds() {
+    return this.state.timeRemaining - (this.hours() * 3600) - (this.minutes() * 60)
+  }
+
 
   tick() {
     this.setState((previousState) => {
@@ -22,7 +43,7 @@ export default class CountdownTimer extends Component {
   }
 
   componentDidMount() {
-    this.setState({ timeRemaining: this.props.timeRemaining })
+    this.setState({ timeRemaining: this.props.timeRemaining * 86400 })
     this.interval = setInterval(this.tick, 1000)
   }
 
@@ -34,7 +55,7 @@ export default class CountdownTimer extends Component {
     return (
       <div>
         Time until expiration:
-        <div>{this.state.timeRemaining}</div>
+        <div>{this.days()} days, {this.hours() % 24} hours, {this.minutes()} minutes, {this.seconds()} seconds</div>
       </div>
     )
   }
